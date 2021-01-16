884862b216e5ef0d0fdb1ecb49bb0debd7c2cffa
PhyaeSoneThwim <pyaesonethwim1998@gmail.com>
2021-01-16 10:39:44 +0630

    added documentation for order routes

--- src/routes/order.js
+++ src/routes/order.js
@@ -4,23 +4,26 @@ const restrictTo = require("../middlewares/restrictTo");
 const orderController = require("../controllers/order");
 const router = express.Router();
 
+// authorize permissions for admin & super-admin
+const authorize = restrictTo("admin", "super-admin");
+
+// only authenticated user can access all defined routes
 router.use(protect);
 
+// route defined to get user's orders
 router.get("/my-orders", orderController.getMyOrders);
 
+// route to make user's order payment
 router.put("/:id/pay", restrictTo("user"), orderController.updateOrderToPaid);
 
-router.put(
-  "/:id/deliver",
-  restrictTo("admin", "super-admin"),
-  orderController.updateOrderToDelivered
-);
+// route to make order successfully delivered to user
+router.put("/:id/deliver", authorize, orderController.updateOrderToDelivered);
 
 router.route("/").post(orderController.addOrder).get(orderController.getOrders);
 
 router
   .route("/:id")
   .get(orderController.getOrder)
-  .delete(restrictTo("admin", "super-admin"), orderController.deleteOrder);
+  .delete(authorize, orderController.deleteOrder);
 
 module.exports = router;
